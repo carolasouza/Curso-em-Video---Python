2d85a2efcf4ac500b53ea8c8913bb98ecef62897
Maria Caroline de Araujo Souza <90577046+carolasouza@users.noreply.github.com>
2022-03-15 18:08:32 -0300

    Delete ex013.py

--- ex013.py
+++
@@ -1,6 +0,0 @@
-print('===== DESAFIO 13 =====')
-print('Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.')
-
-sal = float(input('\nDigite o salário: '))
-aumento = 15/100
-print('\nO salário com aumento será de: R${:.2f}'.format(sal * (1 + aumento)))
