f1bb3c760253c37066b6a1ca00bccd280f11937e
Maria Caroline de Araujo Souza <90577046+carolasouza@users.noreply.github.com>
2022-03-15 18:08:22 -0300

    Delete ex012.py

--- ex012.py
+++
@@ -1,7 +0,0 @@
-print('===== DESAFIO 12 =====')
-print('Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.')
-
-preco = float(input('\nDigite o preço do produto: R$'))
-desconto = 5/100
-
-print('\nDesconto: {}%\nPreço com desconto: R${:.2f}'.format(desconto * 100, preco * (1 - desconto)))
